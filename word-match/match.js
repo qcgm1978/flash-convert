bf041bd0aeb29ea85b49163243432538e79849b0
qcgm1978 <zhanghl@strongunion.com.cn>
2016-07-06 17:51:57 +0800

    no comment

--- word-match/match.js
+++ word-match/match.js
@@ -1,16 +1,30 @@
 $(function () {
-    var words = ["way", "bay", "tail", "day", "play"];
+    var words = ["cave", "skate", "lake", "plane", "rainy"];
     $('.chart:even .img div').css('background-image', function (i, n) {
         return 'url(images/' +
             (i + 1) +
             '.png)';
     })
     $('.left .img:gt(4)').after(function (i, n) {
-        var $div=$('<div>')
-        var $img=$('<img>',{
-            src:'images/1110001.png'
+        var $div = $('<div>')
+        var word = words[i];
+        var $img = $('<img>', {
+            src: 'images/1110001.png',
+            alt: word
         });
-        $div.append($img).append($('<div>').text(words[i]))
+        $div.append($img).append($('<div>').text(word))
         return $div;
     })
+    $('[src="images/1110001.png"]').click({},function (evt) {
+        var word = $(this).attr('alt');
+        if ($.isEmptyObject(evt.data[word])) {
+            var audioElement = document.createElement('audio');
+            audioElement.setAttribute('src', 'sounds/' + word + '.mp3');
+            audioElement.setAttribute('autoplay', 'autoplay');
+            evt.data[word] = audioElement
+            audioElement.play();
+        } else {
+            evt.data[word].play();
+        }
+    });
 });
