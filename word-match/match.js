719aedba75c62632c1967beddd38ee9038eed51d
qcgm1978 <zhanghl@strongunion.com.cn>
2016-07-06 18:35:05 +0800

    no comment

--- word-match/match.js
+++ word-match/match.js
@@ -6,17 +6,19 @@ $(function () {
             '.png)';
     })
     $('.left .img:gt(4)').after(function (i, n) {
-        var $div = $('<div>')
         var word = words[i];
-        var $img = $('<img>', {
-            src: 'images/1110001.png',
-            alt: word
-        });
-        $div.append($img).append($('<div>').text(word))
+        var $div = $('<div>')
+            .css({
+            backgroundImage:'url("images/1110001.png")'
+        })
+            .attr('word',word)
+
+        $div.append($('<div>').text(word))
         return $div;
     })
-    $('[src="images/1110001.png"]').click({},function (evt) {
-        var word = $(this).attr('alt');
+    $('[word]').click({},function (evt) {
+        $(this).addClass('sound')
+        var word = $(this).attr('word');
         if ($.isEmptyObject(evt.data[word])) {
             var audioElement = document.createElement('audio');
             audioElement.setAttribute('src', 'sounds/' + word + '.mp3');
