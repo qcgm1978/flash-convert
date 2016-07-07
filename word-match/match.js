a7f32726706dee30661e9c8d839269e2206cfc04
qcgm1978 <zhanghl@strongunion.com.cn>
2016-07-07 11:52:15 +0800

    no comment

--- word-match/match.js
+++ word-match/match.js
@@ -1,32 +1,58 @@
 $(function () {
     var words = ["cave", "skate", "lake", "plane", "rainy"];
+    var randomArr=shuffle([...Array(words.length).keys()])
     $('.chart:even .img div').css('background-image', function (i, n) {
+        $(this).attr('word',words[i])
         return 'url(images/' +
-            (i + 1) +
+            (randomArr[i] + 1) +
             '.png)';
     })
+    function shuffle(array) {
+        var currentIndex = array.length, temporaryValue, randomIndex;
+
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+
+        return array;
+    }
     $('.left .img:gt(4)').after(function (i, n) {
         var word = words[i];
         var $div = $('<div>')
             .css({
-            backgroundImage:'url("images/1110001.png")'
-        })
-            .attr('word',word)
-
+                backgroundImage: 'url("images/1110001.png")'
+            })
+            .attr('word', word)
         $div.append($('<div>').text(word))
         return $div;
     })
-    $('[word]').click({},function (evt) {
-        $(this).addClass('sound')
-        var word = $(this).attr('word');
-        if ($.isEmptyObject(evt.data[word])) {
-            var audioElement = document.createElement('audio');
-            audioElement.setAttribute('src', 'sounds/' + word + '.mp3');
-            audioElement.setAttribute('autoplay', 'autoplay');
-            evt.data[word] = audioElement
-            audioElement.play();
+    $('.img').click({}, function (evt) {
+        if ($(this).parents('.word').length>0) {
+            if ($(this).is('[word]')) {
+                $(this).addClass('sound')
+                var word = $(this).attr('word');
+                if ($.isEmptyObject(evt.data[word])) {
+                    var audioElement = document.createElement('audio');
+                    audioElement.setAttribute('src', 'sounds/' + word + '.mp3');
+                    audioElement.setAttribute('autoplay', 'autoplay');
+                    evt.data[word] = audioElement
+                    audioElement.play();
+                } else {
+                    evt.data[word].play();
+                }
+            }
         } else {
-            evt.data[word].play();
         }
+        $('.img').removeClass('shadow');
+        $(this).addClass('shadow')
     });
 });
