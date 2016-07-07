e65935c76a04ca4351be8a0696fa7cbfa69ba091
qcgm1978 <zhanghl@strongunion.com.cn>
2016-07-07 15:41:41 +0800

    no comment

--- word-match/match.js
+++ word-match/match.js
@@ -1,30 +1,28 @@
 $(function () {
     var words = ["cave", "skate", "lake", "plane", "rainy"];
-    var randomArr=shuffle([...Array(words.length).keys()])
+    var randomArr = shuffle([...Array(words.length).keys()])
     $('.chart:even .img div').css('background-image', function (i, n) {
-        $(this).attr('word',words[i])
+        $(this).attr('word', words[i])
         return 'url(images/' +
             (randomArr[i] + 1) +
             '.png)';
     })
     function shuffle(array) {
         var currentIndex = array.length, temporaryValue, randomIndex;
-
         // While there remain elements to shuffle...
         while (0 !== currentIndex) {
 
             // Pick a remaining element...
             randomIndex = Math.floor(Math.random() * currentIndex);
             currentIndex -= 1;
-
             // And swap it with the current element.
             temporaryValue = array[currentIndex];
             array[currentIndex] = array[randomIndex];
             array[randomIndex] = temporaryValue;
         }
-
         return array;
     }
+
     $('.left .img:gt(4)').after(function (i, n) {
         var word = words[i];
         var $div = $('<div>')
@@ -36,7 +34,7 @@ $(function () {
         return $div;
     })
     $('.img').click({}, function (evt) {
-        if ($(this).parents('.word').length>0) {
+        if ($(this).parents('.word').length > 0) {
             if ($(this).is('[word]')) {
                 $(this).addClass('sound')
                 var word = $(this).attr('word');
@@ -49,10 +47,16 @@ $(function () {
                 } else {
                     evt.data[word].play();
                 }
+            } else {
+                $(this)
+                    .addClass('move move-name')
+                    .removeClass('shadow')
+                    .next()
+                    .addClass('move trumpet');
             }
         } else {
+            $('.img').removeClass('shadow');
+            $(this).addClass('shadow')
         }
-        $('.img').removeClass('shadow');
-        $(this).addClass('shadow')
     });
 });
